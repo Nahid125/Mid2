eaef4da6346f26af6ea8cf17c33b78fce75cbb36
Nahid125 <nahid.clash76@gmail.com>
2022-11-14 15:00:42 +0400

    first commit

--- atestasiya.js
+++ atestasiya.js
@@ -1,34 +1,30 @@
 class Message {
-    constructor(author, time, text) {
-      this.muellif = muellif;
-      this.tarix = tarix;
-      this.text = text;
-    }
-    toString() {
-        console.log(`${this.tarix} ${this.muellif}: ${this.text}`)
-      }
-    }
-    
-    class Messenger {
-      constructor() {
-        this.history = [];
-      }
-    
-      gettime() {
-        let now = new Date();
-        return `${now.getHours()}:${now.getMinutes()}`;
-      }
-    
-      send(author, text) {
-        this.history.push(new Message(author, this.gettime(), text));
-      }
-    
-      show_history() {  
-        this.history.forEach(el=> el.toString())
-      }
-    }
+  constructor(author, time, text) {
+    this.author = author;
+    this.time = time;
+    this.text = text;
+  }
+  toString() {
+    console.log(`${this.time} ${this.author}: ${this.text}`)
+  }
+}
+class Messenger {
+  constructor() {
+    this.history = [];
+  }
+  send(author, text) {
+    this.history.push(new Message(author, this.gettime(), text));
+  }
+  gettime() {
+    let now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;
+  }
+  show_history() {  
+    this.history.forEach(el=> el.toString())
+  }
+}
 let messenger = new Messenger()
 messenger.send('Adil', 'ilk mesaj')
 messenger.send('Məryəm', 'İkinci mesaj')
-messenger.send("Fuad","Salam")
+messenger.send("Kenan","Salam")
 messenger.show_history()
